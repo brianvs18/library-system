fa2566a2bc1f84d3d2853c96b38288c00779a3fd
Brian Vanegas <brianvs18@gmail.com>
2024-05-16 11:44:28 -0500

    Add attributes in models

---
+++ model/book.go
@@ -0,0 +1,9 @@
+package model
+
+type Book struct {
+	Id        string `json:"id"`
+	Title     string `json:"title"`
+	AuthorId  string `json:"author_id"`
+	Publisher string `json:"publisher"`
+	Genre     string `json:"genre"`
+}
---
+++ model/loan.go
@@ -0,0 +1,9 @@
+package model
+
+type Loan struct {
+	Id         string `json:"id"`
+	BookId     string `json:"book_id"`
+	UserId     string `json:"user_id"`
+	LoanDate   string `json:"loan_date"`
+	ReturnDate string `json:"return_date"`
+}
